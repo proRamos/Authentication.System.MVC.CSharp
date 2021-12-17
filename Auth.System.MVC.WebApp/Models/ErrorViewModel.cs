50334df5af2f6b7a191de0141747ef92331148f7
Protásio Ramos <ramosprotasio@gmail.com>
2021-12-17 12:12:42 +0000

    Inicial commit

---
+++ Auth.System.MVC.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Auth.System.MVC.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
